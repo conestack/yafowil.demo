8bc8b04d28bb6bb7a8ab7ae3132d6eee775dfb8d
Robert Niederreiter <office@squarewave.at>
2012-06-12 14:14:59 +0200

    add widget

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='yafowil.demo',
         'yafowil.yaml',
          # add-on widgets to document
         'yafowil.widget.richtext',
-        #'yafowil.widget.wysihtml5',
+        'yafowil.widget.wysihtml5',
         'yafowil.widget.datetime',   
         'yafowil.widget.array',
         'yafowil.widget.dict',
