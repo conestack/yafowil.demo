69ac11b7b797541d01b80a17c4b938fde271bcdf
Robert Niederreiter <office@squarewave.at>
2019-09-10 15:47:53 +0200

    no dynatree and recapchs for now

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
-rm -r ./lib ./include ./local ./bin
-virtualenv --clear .
+rm -r ./lib ./include ./local ./bin ./share
+virtualenv -p python3 --clear .
 ./bin/pip install --upgrade pip setuptools zc.buildout
 ./bin/buildout -c localhost.cfg
