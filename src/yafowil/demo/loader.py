d64e0067c429471d150bb63d0cbbdc3e4afa7c06
Robert Niederreiter <rnix@squarewave.at>
2022-05-23 14:46:07 +0200

    Add webresource resource registration

--- src/yafowil/demo/loader.py
+++ src/yafowil/demo/loader.py
@@ -2,10 +2,38 @@ from yafowil import bootstrap
 from yafowil.base import factory
 from yafowil.utils import entry_point
 import os
+import webresource as wr
 
 
 resources_dir = os.path.join(os.path.dirname(__file__), 'resources')
 
+
+##############################################################################
+# Default
+##############################################################################
+
+# webresource ################################################################
+
+scripts = wr.ResourceGroup(name='scripts')
+scripts.add(wr.ScriptResource(
+    name='jquery-js',
+    directory=resources_dir,
+    resource='jquery-3.6.0.js',
+    compressed='jquery-3.6.0.min.js'
+))
+
+styles = wr.ResourceGroup(name='styles')
+styles.add(wr.StyleResource(
+    name='yafowil-demo-css',
+    directory=resources_dir,
+    resource='yafowil.demo.css'
+))
+
+resources = wr.ResourceGroup(name='demo-resources')
+resources.add(scripts)
+resources.add(styles)
+
+# B/C resources ##############################################################
 
 js = [{
     'group': 'yafowil.demo.dependencies',
@@ -19,13 +47,21 @@ css = [{
 }]
 
 
+##############################################################################
+# Registration
+##############################################################################
+
 @entry_point(order=10)
 def register():
     factory.register_theme(
         'bootstrap', 'yafowil.demo', resources_dir,
-        js=js, css=css
+        js=js, css=css, resources=resources
     )
 
+
+##############################################################################
+# Configuration
+##############################################################################
 
 def configure():
     bootstrap.configure_factory('bootstrap')
